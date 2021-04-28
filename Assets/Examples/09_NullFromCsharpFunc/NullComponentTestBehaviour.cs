ae2c3b3dc8e7892af6a1d3c480446af1b4a1976f
Cheolhui <cheolhui.lee@naverz-corp.com>
2021-04-28 15:30:30 +0900

    added sample of c# null test.

---
+++ Assets/Examples/09_NullFromCsharpFunc/NullComponentTestBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Puerts;
+using UnityEngine;
+
+namespace PuertsTest
+{
+    public class NullComponentTestBehaviour : MonoBehaviour
+    {
+        public delegate void ModuleInit(NullComponentTestBehaviour monoBehaviour);
+
+        public string ModuleName;
+
+        public Action JsStart;
+
+        static JsEnv jsEnv;
+
+        void Awake()
+        {
+            if (jsEnv == null) jsEnv = new JsEnv();
+
+            var init = jsEnv.Eval<ModuleInit>("const m = require('" + ModuleName + "'); m.init;");
+
+            if (init != null) init(this);
+        }
+
+        void Start()
+        {
+            if (JsStart != null) JsStart();
+        }
+
+        void OnDestroy()
+        {
+            JsStart = null;
+        }
+    }
+}
